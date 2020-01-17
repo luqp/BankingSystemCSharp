7562b348cfb5152f932091a34407ad362297db8e
Luqp <luceroqp@outlook.com>
2020-01-16 21:53:51 -0400

    Organize code in a solution, and add a basic bank program

---
+++ src/Bank/BusinessLogic/BankService.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace BankingSystem.BusinessLogic
+{
+    public class BankService
+    {
+        private Bank bank;
+
+        public BankService()
+        {
+            this.bank = new Bank();
+        }
+
+        public int GetBalance(int accountNumber)
+        {
+            BankAccount bankAccount = bank.GetBankAccount(accountNumber);
+            return bankAccount.Balance;
+        }
+
+        public int newAccount(string accountOrigin)
+        {
+            if (accountOrigin.Equals(""))
+            {
+                return bank.NewAccount();
+            }
+
+            if (Enum.TryParse(accountOrigin, true, out AccountOrigin origin))
+            {
+                return bank.NewAccount(origin);
+            }
+            
+            throw new ArgumentException($"<{accountOrigin}> is not a valid origin.");
+        }
+
+        public void Deposit(int accountNumber, int amount)
+        {
+            BankAccount bankAccount = bank.GetBankAccount(accountNumber);
+            bankAccount.Deposit(amount);
+        }
+
+        public bool AuthorizeLoan(int accountNumber, int loanAmount)
+        {
+            BankAccount bankAccount = bank.GetBankAccount(accountNumber);
+            // TODO: intermediate checks
+            return bankAccount.HasEnoughCollateral(loanAmount);
+        }
+
+        public void PayInterest()
+        {
+            bank.PayInterest();
+        }
+
+        public string GetBankInformation()
+        {
+            return bank.ToString();
+        }
+    }
+}
---
+++ src/BankConsole/BankClient.cs
@@ -0,0 +1,135 @@
+using System;
+using BankingSystem.BusinessLogic;
+
+namespace BankingSystem
+{
+    public class BankClient
+    {
+        private bool done;
+        private int currentAccount;
+        private BankService bankService;
+
+        public BankClient()
+        {
+            this.done = false;
+            this.currentAccount = 0;        
+        }
+
+        public void run()
+        {
+            
+            bankService = new BankService();
+            while(!done)
+            {
+                Console.Write("Enter command (0=quit, 1=new, 2=select, 3=deposit, 4=loan, 5=show, 6=interest): ");
+                string input = Console.ReadLine();
+                
+                if(!Int32.TryParse(input, out int commandNumber))
+                {
+                    commandNumber = Int32.MaxValue;
+                }
+
+                try
+                {
+                    ProcessCommand(commandNumber);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void ProcessCommand(int commandNumber)
+        {
+            switch (commandNumber)
+            {
+                case 0:
+                    quit();
+                    break;
+                case 1:
+                    newAccount();
+                    break;
+                case 2:
+                    select();
+                    break;
+                case 3:
+                    deposit();
+                    break;
+                case 4:
+                    authorizeLoan();
+                    break;
+                case 5:
+                    showAll();
+                    break;
+                case 6:
+                    addInterest();
+                    break;
+                default:
+                    Console.WriteLine("Illegal command");
+                    break;
+            }
+        }
+
+        private void quit()
+        {
+            done = true;
+            Console.WriteLine("Byeee...");
+        }
+
+        private void newAccount()
+        {
+            Console.Write("Specify the origin of the account (Local, Rural, Foreign): ");
+            string accountOrigin = Console.ReadLine();
+            currentAccount = bankService.newAccount(accountOrigin);
+            Console.WriteLine("Your new account number is: " + currentAccount);
+        }
+
+        private void select()
+        {
+            Console.Write("Enter account#: ");
+            string input = Console.ReadLine();
+            Int32.TryParse(input, out currentAccount);
+            
+            int balance = bankService.GetBalance(currentAccount);
+            Console.WriteLine($"The balance of account {currentAccount} is {balance}");
+        }
+
+        private void deposit()
+        {
+            Console.Write("Enter deposit amount: ");
+            string input = Console.ReadLine();
+            Int32.TryParse(input, out int amount);
+            
+            bankService.Deposit(currentAccount, amount);
+        }
+
+        private void authorizeLoan()
+        {
+            Console.Write("Enter loan amount: ");
+            string input = Console.ReadLine();
+            Int32.TryParse(input, out int loanAmount);
+
+            if (bankService.AuthorizeLoan(currentAccount, loanAmount))
+            {
+                Console.WriteLine("The loan was approved");
+            }
+            else
+            {
+                Console.WriteLine("The loan was denied");
+            }
+        }
+
+        private void showAll()
+        {
+            Console.WriteLine(bankService.GetBankInformation());
+        }
+
+        private void addInterest()
+        {
+            bankService.PayInterest();
+            Console.WriteLine("Interes were payed.");
+        }
+    }
+
+}
